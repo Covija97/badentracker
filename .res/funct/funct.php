01fce8d27042d93690ceaa0d1b4173283ca3eece
Covija97 <48259969+Covija97@users.noreply.github.com>
2025-05-15 20:24:03 +0200

    Update server setup instructions and fix database connection logic

--- .res/funct/funct.php
+++ .res/funct/funct.php
@@ -6,7 +6,16 @@ function linkDB ():mysqli  {
     $username = "root";
     $password = "";
     
-    return new mysqli($servername, $username, $password, $database);
+    $conx new mysqli($servername, $username, $password, $database);
+
+    if($conx->connect_errno) {
+        printf("Connect failed: %s\n", $conx->connect_error);
+        exit();
+    } else {
+        return $conx;
+        $db = mysqli_select_db($conx, $db);
+    }
+
 }
 
 function getTable(): string {
