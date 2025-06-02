420d8fc0b901a1ca02c3e5302cbc916abb016952
Covija97 <48259969+Covija97@users.noreply.github.com>
2025-06-02 20:32:18 +0200

    Refactor PDF class structure and improve header/footer methods for clarity

--- reuniones/reunion/test.php
+++ reuniones/reunion/test.php
@@ -8,10 +8,37 @@ require "../../.res/funct/funct.php";
 ?>
 
 <?php
+class PDF extends FPDF
+{
+
+    // Cabecera de página
+    function Header(): void
+    {
+        // Logo
+        $this->Image('../../.res/fpdf/img/header.png', null, null, 170);
+        // Arial bold 15
+        $this->SetFont('Arial', 'B', 15);
+        $this->Ln(7);
+    }
+
+    // Pie de página
+    function Footer(): void
+    {
+        // Posición: a 1,5 cm del final
+        $this->SetY(-15);
+        // Arial italic 8
+        $this->SetFont('Arial', '', 10);
+        // Número de página
+        $this->Cell(0, 10, 'Página ' . $this->PageNo() . '/{nb}', 0, 0, 'R');
+    }
+}
+?>
+
+<?php
 $pdf = new PDF('P', 'mm', 'A4');
 $pdf->SetMargins(20, 15, 20);
 $pdf->AliasNbPages();
 $pdf->AddPage();
-$pdf->SetFont( 'Times', '', 12);
+$pdf->SetFont('Times', '', 12);
 
 ?>
