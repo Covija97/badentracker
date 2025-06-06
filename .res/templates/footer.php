74b44a668701b0e461d32d47a51da1251a80cf95
Covija97 <48259969+Covija97@users.noreply.github.com>
2025-06-06 18:16:54 +0200

    footerupd

--- .res/templates/footer.php
+++ .res/templates/footer.php
@@ -1,6 +1,6 @@
     <script src="/.res/js/scripts.js?v=2" defer></script>
     <footer>
-        <p>BadenTracker &copy; 2025</p>
+        <p>BadenTracker 2025 <i>by Javier Coronel Vides</i> </p>
     </footer>
 </body>
 </html>
