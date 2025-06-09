fa163e2f988ace8a8b7f440353f078a956cd7295
Covija97 <48259969+Covija97@users.noreply.github.com>
2025-06-09 13:17:38 +0200

    fix: simplify footer link in PDF export for cleaner output

--- reuniones/reunion/pdfExport.php
+++ reuniones/reunion/pdfExport.php
@@ -125,9 +125,7 @@ class PDF extends FPDF
         // Arial italic 8
         $this->SetFont('Arial', '', 10);
         // Hecho con el logo de la web
-        $webLink = 'BadenTracker';
-        $this->Link($webLink, 'https://www.badentracker.click');
-        $this->Cell(0, 10, utf8_decode('Hecho con ' . $webLink), 0, 0, 'L');
+        $this->Cell(0, 10, utf8_decode('Hecho con BadenTracker'), 0, 0, 'L');
         // Número de página
         $this->Cell(0, 10, utf8_decode('Página ' . $this->PageNo() . '/{nb}'), 0, 0, 'R');
     }
