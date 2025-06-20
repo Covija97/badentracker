31790bdf6b0fa8b24445e417d39f4f3bf89ad9af
Covija97 <48259969+Covija97@users.noreply.github.com>
2025-06-20 13:34:37 +0200

    fix: update logout link text to Spanish for consistency

--- .res/templates/header.php
+++ .res/templates/header.php
@@ -57,7 +57,7 @@ if (!isset($_SESSION['user_id'])) {
             ?>
             >Calendario</a>
             <?php if (isset($_SESSION['user_id'])): ?>
-                <a href="/logout.php" title="Cerrar sesión" style="color:#d9534f;font-weight:bold;margin-left:20px;">Log out</a>
+                <a href="/logout.php" title="Cerrar sesión" style="color:#d9534f;font-weight:bold;margin-left:20px;">Cerrar sesión</a>
             <?php endif; ?>
         </div>
     </header>
