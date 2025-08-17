bf0045378559a323185de18286b1b1fcfd277312
Covija97 <covija97@gmail.com>
2025-08-17 13:18:53 +0200

    fix: update script source path in footer.php for consistency

--- .res/templates/footer.php
+++ .res/templates/footer.php
@@ -1,4 +1,4 @@
-    <script src="/.res/js/scripts.js?v=2" defer></script>
+    <script src="/badentracker/.res/js/scripts.js?v=2" defer></script>
     <footer>
         <p>BadenTracker 2025 by <b>Javier Coronel Vides</b></p>
     </footer>
